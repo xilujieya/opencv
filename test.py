2d2ae3a5112d2e38983ad16dbcd261303728be3c
pat_liu <1820075491@qq.com>
2023-07-04 22:46:33 +0800

    版本管理文件

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2023/7/4 22:33
+# @Author  : pat
+# @FileName: test.py
+# @Software: PyCharm
+
+from numpy import *
+eye(4)
+array([[1., 0., 0., 0.],
+       [0., 1., 0., 0.],
+       [0., 0., 1., 0.],
+       [0., 0., 0., 1.]])
